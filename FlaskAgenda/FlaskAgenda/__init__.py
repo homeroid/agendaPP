522dda3eed93b267e655a4a2e66691eb0d686bc0
homeroid <ome_gonza@hotmail.com>
2019-04-05 22:36:28 -0700

    inti

--- FlaskAgenda/FlaskAgenda/__init__.py
+++ FlaskAgenda/FlaskAgenda/__init__.py
@@ -2,7 +2,9 @@
 The flask application package.
 """
 
-from flask import Flask
+from flask import Flask, request ,redirect 
+
+
 app = Flask(__name__)
 
 import FlaskAgenda.views
--- FlaskAgenda/FlaskAgenda/views.py
+++ FlaskAgenda/FlaskAgenda/views.py
@@ -3,8 +3,15 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from FlaskAgenda import app
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_HOST'] = 'agenda_phone'
+
+mysql = MySQL(app)
 
 @app.route('/')
 @app.route('/home')
@@ -35,3 +42,17 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+
+@app.route('/insert',methods = ['POST'])
+def insert():
+
+        if request.method == "POST":
+            name = request.form['name']
+            lastname = request.form['lastname']
+            email = request.form['email']
+            number= request.form['number']
+            type = request.form['type']
+
+            cur = mysql.connection.cursor()
+            cur.execute("INSERT INTO agenda (name, lastname, email, number, type) VALUES (%s, %s, %s) ", (name, lastname, email, number, type)
+            return redirect(url_for('Index'))
